6d6b4e9f50ea47deecd4e906194a483dc6f247f8
Josh-Murray <joshua02murray@gmail.com>
2020-02-24 17:43:02 +1100

    Fixed scraper.js to scrape globalincidentmap instead of who

--- PHASE_1/API_SourceCode/scraper.js
+++ PHASE_1/API_SourceCode/scraper.js
@@ -1,20 +1,20 @@
 const axios = require('axios');
 const cheerio = require('cheerio')
-const base_whs = 'https://www.who.int'
+const base_url = 'http://outbreaks.globalincidentmap.com/'
 // Grab the page
-axios(base_whs + '/emergencies/diseases/en/')
+axios(base_url)
 	.then(response => {
 		const html = response.data;
 		const $ = cheerio.load(html)
 		const pages = [];
 
 		// Find the list of recent events
-		const lis = $('.auto_archive > li');
-		// For each event, add the url to the list
-		lis.each(function(){
-			pages.push(base_whs + $(this).find('a').attr('href'))
+		const items = $('#pic1');
+		links = items.find('.b2')
+		links.each(function(){
+			pages.push([base_url + $(this).attr('href'), $(this).text().trim()])
 		})
-		// For each url, grab article information
+		// For each event, add the url to the list
 		return Promise.all(
 			pages.map(url =>{
 				return parseURL(url)
@@ -26,42 +26,23 @@ axios(base_whs + '/emergencies/diseases/en/')
 	.catch(console.error);
 // Grab info from article
 function parseURL(url){
-	return axios(url)
+	return axios(url[0])
 		.then(response =>{
+			data = []
 			const html = response.data;
 			const $ = cheerio.load(html);
-			const meta = $('.meta').children().text().split('\n')[1].split(' ');
-			const date = meta[2] + '-' + monthParse(meta[1]) + '-' + meta[0] + ' xx:xx:xx'
-			const headline = $('.headline').text()
-			const main_text = [];
-			// WHO has an aides website structure, idk if this will work for all articles on their site, but so far so good :shrug
-			$('#primary').children().next().next().each(function(){
-				if ($(this).is('p')){
-					main_text.push($(this).text().replace(/(\\[tn])/g, "").trim())
-				}else{
-					return false;
-				}
+			$('.tdline').filter(function(i, el){
+				return $(this).text().trim() !== ""
 			})
-			return {url: url, date_of_publication: date, headline: headline, main_text: main_text.join('\n')}
+			.each(function(){
+				data.push($(this).text().trim())
+			})
+			//console.log(data)
+			text = $(".tdtext").text().split("\n").join(" ").replace(/ +(?= )/g,'').trim()
+			return {url: url[0], date_of_publication: data[3],
+				headline: url[1], main_text: text}
 
 		})
 		.catch(console.error)
 }
-// gOoD cOdInG
-function monthParse(mon){
-	var months = {
-		'january' : '01',
-		'february' : '02',
-		'march' : '03',
-		'april' : '04',
-		'may' : '05',
-		'june' : '06',
-		'july' : '07',
-		'august' : '08',
-		'september' : '09',
-		'october' : '10',
-		'november' : '11',
-		'december' : '12'
-	}
-	return months[mon.toLowerCase()]
-}
+
