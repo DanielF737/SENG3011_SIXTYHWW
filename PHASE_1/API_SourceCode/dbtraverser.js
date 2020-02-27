3f774580c01c10198a355b577fddfece4c68c651
Nikil <nikil.s537@gmail.com>
2020-02-27 17:48:24 +1100

    Started retrieval queries.

--- PHASE_1/API_SourceCode/dbtraverser.js
+++ PHASE_1/API_SourceCode/dbtraverser.js
@@ -17,6 +17,19 @@ function saveArticle(url, headline, main_text, date_of_pub) {
   // Inserts the article to the database.
   db.run(query);
 
+  /*
+  let lastArticle = -1;  
+  let lastArticleQuery = 'SELECT id FROM Article WHERE id = (SELECT MAX(ID) FROM Article);';
+  db.all(lastArticleQuery, [], (err, rows) => {
+    if (err) {
+      throw err;
+    }
+    rows.forEach((row) => {
+      lastArticle = row.id;
+    });
+  });
+  */
+
   // Closes the database.
   db.close((err) => {
     if (err) {
@@ -84,14 +97,47 @@ function savePartOf(articleID, reportID) {
 
 // Retrieve X amount of articles from the database.
 function retrieveArticles(amount) {
+  const sqlite3 = require('sqlite3').verbose();
 
+  // Opens up the database.
+  let db = new sqlite3.Database('./database', (err) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log('Connected to the database.');
+  });
+
+  // Closes the database.
+  db.close((err) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log('Close the database connection.');
+  });
 }
 
 // Retrieves X amount of reports from database.
 function retrieveReports(amount) {
+  const sqlite3 = require('sqlite3').verbose();
 
+  // Opens up the database.
+  let db = new sqlite3.Database('./database', (err) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log('Connected to the database.');
+  });
+
+  // Closes the database.
+  db.close((err) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log('Close the database connection.');
+  });
 }
 
-saveArticle("google.com", "corona", "stuff happened", "2020-12-12");
+let a = saveArticle("google.com", "corona", "stuff happened", "2020-12-12");
+console.log(a);
 saveReport("Corona", "Stuff", "2000-12-12", "Sydney");
-savePartOf(1,1);
+//savePartOf(1,1);
