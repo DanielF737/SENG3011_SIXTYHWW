a46146ce6a0a0da7708688b9a8e204b63f227aaf
Nikil <nikil.s537@gmail.com>
2020-03-02 19:03:06 +1100

    Updated schema.

--- PHASE_1/API_SourceCode/schema.sql
+++ PHASE_1/API_SourceCode/schema.sql
@@ -17,3 +17,30 @@ create table if not exists Report (
   location    text,
   foreign key (article_id) references Article(id)
 );
+
+create table if not exists Diseases (
+  disease     text not null,
+  primary key (disease)
+);
+
+create table if not exists Syndromes (
+  id          integer primary key autoincrement,
+  syndrome    text not null
+);
+
+create table if not exists Users (
+  id          integer primary key autoincrement,
+  name        text not null,
+  username    text not null, 
+  password    text not null,
+  email       text not null,
+  phone       text
+);
+
+create table if not exists SubbedDiseases (
+  user_id     integer not null,
+  disease     text not null,
+  primary key (user_id, disease_id),
+  foreign key (user_id) references Users(id),
+  foreign key (disease) references Diseases(disease)
+);
