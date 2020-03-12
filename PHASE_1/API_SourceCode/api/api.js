6c35a767086f71cad970ca6f7d19b4e44e104b9f
Josh-Murray <joshua02murray@gmail.com>
2020-03-12 23:08:53 +1100

    Refactored the API design, updated swagger to follow new design

--- PHASE_1/API_SourceCode/api/api.js
+++ PHASE_1/API_SourceCode/api/api.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const db = require('../common/db');
-
+const YAML = require("yamljs")
+const swagger = require('swagger-ui-express')
+const swaggerDoc = YAML.load("../../API_Documentation/swag.yml")
 // Constants
 const PORT = 3000;
 
@@ -10,19 +12,22 @@ const app = express();
 
 // Middleware
 app.use(bodyParser.json());
-
-/* 
+/*
   Routes
 */
 
 // Root
-app.get('/', (req, res) => res.send('Machine Learning API'));
+app.use('/', swagger.serve)
+app.get('/', swagger.setup(swaggerDoc))
 
-// Search Route
 app.get('/search', async (req, res) => {
   const articles = await db.search(req.body);
   res.send(articles);
 });
+/**
+ * @swagger
+ *
+*/
 
 app.get('/reports', async (req, res) => {
   const articles = await db.getReports();
