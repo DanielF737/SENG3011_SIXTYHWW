75ad283f841d53a04b726fa3c2fb9a5a628429d1
Ninjachicken737 <daniel@ferra.ro>
2020-03-13 10:58:33 +1100

    modified api code directory structure

---
+++ PHASE_1/API_SourceCode/api.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const db = require('common/db');
+const YAML = require("yamljs")
+const swagger = require('swagger-ui-express')
+const swaggerDoc = YAML.load("../API_Documentation/swag.yml")
+// Constants
+const PORT = 3000;
+
+// Init Express
+const app = express();
+
+// Middleware
+app.use(bodyParser.json());
+/*
+  Routes
+*/
+
+// Root
+app.use('/', swagger.serve)
+app.get('/', swagger.setup(swaggerDoc))
+
+app.get('/search', async (req, res) => {
+  const articles = await db.search(req.body);
+  res.send(articles);
+});
+/**
+ * @swagger
+ *
+*/
+
+app.get('/reports', async (req, res) => {
+  const articles = await db.getReports();
+  res.send(articles);
+});
+
+// Run Server
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
