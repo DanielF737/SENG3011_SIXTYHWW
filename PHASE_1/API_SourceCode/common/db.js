2c07cff7cbb30780521c6be85b5c8e0c24a96f34
Nikil <nikil.s537@gmail.com>
2020-03-22 15:51:46 +1100

    Made minor changes to testing.

--- PHASE_1/API_SourceCode/common/db.js
+++ PHASE_1/API_SourceCode/common/db.js
@@ -1,19 +1,17 @@
 const sqlite = require('sqlite');
 
-const DB_NAME = "../database/database_static.sqlite";	
-const db = sqlite.open(DB_NAME);
-
-/*
-  Scraper
-*/
+var conn;
 
 async function addArticle(article) {
-  const conn = await db;
-
   const statement = await conn.run(`
-    INSERT OR REPLACE
-    INTO articles (url, headline, body, date_of_publication)
-    VALUES ($url, $headline, $body, $date)`, {
+    INSERT INTO articles (url, headline, body, date_of_publication)
+    VALUES ($url, $headline, $body, $date)
+    ON CONFLICT (url)
+    DO UPDATE SET
+    headline = excluded.headline,
+    body = excluded.body,
+    date_of_publication = excluded.date_of_publication
+  `, {
     $url: article.url,
     $headline: article.headline,
     $body: article.main_text,
@@ -24,14 +22,18 @@ async function addArticle(article) {
 }
 
 async function addReport(id, report) {
-  const conn = await db;
-
-  console.log(report);
-  
   const statement = await conn.run(`
-    INSERT OR REPLACE
-    INTO reports (article_id, diseases, syndromes, event_date, country, city, latitude, longitude)
+    INSERT INTO reports (article_id, diseases, syndromes, event_date, country, city, latitude, longitude)
     VALUES ($article_id, $disease, $syndrome, $event_date, $country, $city, $latitude, $longitude)
+    ON CONFLICT (diseases, syndromes, event_date, country, city, longitude, latitude)
+    DO UPDATE SET
+    diseases = excluded.diseases,
+    syndromes = excluded.syndromes,
+    event_date = excluded.event_date,
+    country = excluded.country,
+    city = excluded.city,
+    latitude = excluded.latitude,
+    longitude = excluded.longitude
   `, {
     $article_id: id,
     $disease: JSON.stringify(report.diseases),
@@ -44,46 +46,32 @@ async function addReport(id, report) {
   });
 }
 
-/*
-  API
-
-  `
-    SELECT * FROM reports
-    WHERE event_date > $start_date
-    AND event_date < $end_date
-    AND country == $country
-    AND city == $city
-  `
-
-  const articles = await conn.all("SELECT * FROM reports", {
-      $start_date: searchRequest.start_date,
-      $end_date: searchRequest.end_date,
-      $key_terms: searchRequest.key_terms,
-      $country: searchRequest.country,
-      $city: searchRequest.city,
-    });
-*/
-
 async function search(searchRequest) {
   try {
-    const conn = await db;
-
-    searchRequest.key_terms = searchRequest.key_terms.split(",");
-
-    const articles = await conn.all(`
-      SELECT * FROM reports
-      WHERE event_date > $start_date
+    var query = `
+      SELECT * FROM articles, reports
+      WHERE articles.id == reports.article_id
+      AND event_date > $start_date
       AND event_date < $end_date
-      AND country == $country
-      AND city == $city
-    `, {
+    `;
+
+    if (searchRequest.location)
+      query += `AND (country == $location OR city == $location)`;
+
+    const reports = await conn.all(query, {
       $start_date: searchRequest.start_date,
       $end_date: searchRequest.end_date,
-      $country: searchRequest.country,
-      $city: searchRequest.city,
+      $location: searchRequest.location
     });
 
-    return articles;
+    const regex = new RegExp(searchRequest.key_terms ? searchRequest.key_terms.replace(",", "|") : "", "i");
+
+    return reports.filter((report) =>
+      regex.test(report.headline) ||
+      regex.test(report.body) ||
+      regex.test(report.diseases) ||
+      regex.test(report.syndromes)
+    );
   } catch (e){
     console.log(e);
     return [];
@@ -92,20 +80,20 @@ async function search(searchRequest) {
 
 async function getReports() {
   try {
-    const conn = await db;
-
-    const articles = await conn.all("SELECT * FROM reports");
-
-    return articles;
+    return await conn.all("SELECT * FROM reports");
   } catch (e){
     console.log(e);
     return [];
   }
 }
 
-module.exports = {
-  "addArticle": addArticle,
-  "addReport": addReport,
-  "getReports": getReports,
-  "search": search
-};
+module.exports = async () => {
+  conn = await sqlite.open("../database/database.sqlite");
+
+  return {
+    "addArticle": addArticle,
+    "addReport": addReport,
+    "getReports": getReports,
+    "search": search
+  };
+}
--- PHASE_1/API_SourceCode/scraper/loadData.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs')
-const db = require('../common/db');
-
-const data = fs.readFileSync("../data/output.json");
-
-JSON.parse(data).forEach((article) => {
-  db.addArticle(article);
-});
