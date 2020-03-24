ba4f3fe8e80525e14b7698a2633c456cc8088704
Nikil <nikil.s537@gmail.com>
2020-03-24 11:05:11 +1100

    Finished testing.

--- PHASE_1/TestScripts/configure.sh
+++ PHASE_1/TestScripts/configure.sh
@@ -4,3 +4,4 @@ echo "Configuring Files"
 
 # Changes files in API_SourceCode for testing.
 sed -i '' 's/database.sqlite/database_static.sqlite/g' ../API_SourceCode/common/db.js
+cp database_static.sqlite ../API_SourceCode/database/
--- PHASE_1/TestScripts/reconfigure.sh
+++ PHASE_1/TestScripts/reconfigure.sh
@@ -4,3 +4,4 @@ echo "Reconfiguring Files"
 
 # Changes back the files in API_SourceCode for proper use.
 sed -i '' 's/database_static.sqlite/database.sqlite/g' ../API_SourceCode/common/db.js
+rm ../API_SourceCode/database/database_static.sqlite
